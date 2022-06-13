6351e90822ce0aca4d74345c69afb361ef850670
NewakunGithub <106585427+NewakunGithub@users.noreply.github.com>
2022-06-13 15:31:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = © Created By ® Ridho 
 global.author = ® created by DinoBotz v2
-global.wm = '® Ridho'
+global.wm = '® DinoBotz V2'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
